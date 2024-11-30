e9c0318fab041168c3ee48a05b70912881c9a93b
osawa-koki <koki@osawa.dev>
2024-11-30 13:27:01 +0900

    不要なプロパティの削除。

--- provider.tf
+++ provider.tf
@@ -1,7 +1,3 @@
 provider "azurerm" {
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
   features {}
 }
